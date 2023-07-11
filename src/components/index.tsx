98cb1fca3d0c96561ba5cc8f5eaa2f25989f3b57
Виктория <mihailova-vik@mail.ru>
2023-07-11 22:58:05 +0300

    created react components

---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app';
+import { Setting } from './const';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App
+      offersCount = {Setting.OffersCount}
+    />
+  </React.StrictMode>
+);
