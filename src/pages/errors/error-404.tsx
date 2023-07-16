6925bb25592c3f9caa58b90cd09d4f087d88d042
Виктория <mihailova-vik@mail.ru>
2023-07-16 22:17:48 +0300

    404 correction

--- src/pages/errors/error-404.tsx
+++ src/pages/errors/error-404.tsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function ErrorNotFound() : JSX.Element {
   return (
     <section>
-      <h1>error 404. Page not found</h1>
-      <Link to="/">Back to main page</Link>
+      <Logo />
+      <h1 className="offer__name">Error 404. Page not found</h1>
+      <Link className="footer__logo-link" to="/">Back to the main page</Link>
     </section>
   );
 }
